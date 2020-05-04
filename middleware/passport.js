203ea7640dafcea414eeee87e831f9fd8a94e8b4
AlekzandrM <mishenko.aleksandr@gmail.com>
2020-05-04 20:14:05 +0300

    Installed Passport, Passport-jwt for protection of routes

---
+++ middleware/passport.js
@@ -0,0 +1,28 @@
+const JwtStrategy = require('passport-jwt').Strategy // переменная
+const ExtractJwt = require('passport-jwt').ExtractJwt //ф-я
+const mongoose = require('mongoose')
+const User = mongoose.model('users')
+const keys = require('../config/keys')
+
+const options = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: keys.jwt
+}
+
+module.exports = passport => {
+    passport.use(
+        new JwtStrategy(options, async (payload, done) => {
+            try {
+                const user = await User.findById(payload.userId).select('email id')
+
+                if(user) {
+                    done(null, user)
+                } else {
+                    (null, false)
+                }
+            } catch (e) {
+                console.log(e)
+            }
+        })
+    )
+}
