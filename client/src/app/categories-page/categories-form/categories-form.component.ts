21282d112851dec7e8096f0276987e351f6daec7
AlekzandrM <mishenko.aleksandr@gmail.com>
2020-05-10 23:37:40 +0300

    Categories-form. Delete Category

--- client/src/app/categories-page/categories-form/categories-form.component.ts
+++ client/src/app/categories-page/categories-form/categories-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {CategoriesService} from "../../shared/services/categories.service";
 import {switchMap} from "rxjs/operators";
@@ -23,7 +23,8 @@ export class CategoriesFormComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private categoriesService: CategoriesService
+    private categoriesService: CategoriesService,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -75,6 +76,18 @@ export class CategoriesFormComponent implements OnInit {
     }
 
     reader.readAsDataURL(file)
+  }
+
+  deleteCategory() {
+    const decision = window.confirm(`Вы действительно хотите удалить категорию "${this.category.name}"?`)
+    if (decision) {
+      this.categoriesService.delete(this.category._id)
+        .subscribe(
+          category => MaterialService.toast(category.message),
+          error => MaterialService.toast(error.error.message),
+          () => {this.router.navigate(['/categories'])}
+        )
+    }
   }
 
   onSubmit() {
--- client/src/app/shared/interfaces.ts
+++ client/src/app/shared/interfaces.ts
@@ -8,3 +8,7 @@ export interface Category {
   user?: string
   _id?: string
 }
+
+export interface Message {
+  message: string
+}
