dfc6501795595450858365824c24cdc2fc37525d
AlekzandrM <mishenko.aleksandr@gmail.com>
2020-05-11 11:44:13 +0300

    Positions-form. Loading and displaying of positions. Created Positions.service.

--- client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
+++ client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {PositionsService} from "../../../shared/services/positions.service";
+import {Position} from "../../../shared/interfaces";
 
 @Component({
   selector: 'app-positions-form',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./positions-form.component.scss']
 })
 export class PositionsFormComponent implements OnInit {
+  @Input('categoryId') categoryId: string
+  positions: Position[] = []
+  loading = false
 
-  constructor() { }
+  constructor(private positionsService: PositionsService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.loading = true
+    this.positionsService.fetch(this.categoryId).subscribe(positions => {
+      this.positions = positions
+      this.loading = false
+    })
   }
 
 }
