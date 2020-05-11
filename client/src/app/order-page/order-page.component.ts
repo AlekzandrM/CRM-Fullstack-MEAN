052a70fc76f70cafebc3a018893e530f7763b5b1
AlekzandrM <mishenko.aleksandr@gmail.com>
2020-05-11 23:28:13 +0300

    Order-page. Created Breadcrumbs, Modal for order

--- client/src/app/order-page/order-page.component.ts
+++ client/src/app/order-page/order-page.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
+import {MaterialInstance, MaterialService} from "../shared/classes/material.service";
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
   styleUrls: ['./order-page.component.scss']
 })
-export class OrderPageComponent implements OnInit {
+export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor() { }
+  isRoot: boolean
+  modal: MaterialInstance
+  @ViewChild('modal') modalRef: ElementRef
 
-  ngOnInit(): void {
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+    this.isRoot = this.router.url === '/order'
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isRoot = this.router.url === '/order'
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    this.modal.destroy()
+  }
+
+  ngAfterViewInit() {
+    this.modal = MaterialService.initModal(this.modalRef)
+  }
+
+  open() {
+    this.modal.open()
+  }
+
+  cancel() {
+    this.modal.close()
+  }
+
+  submit() {
+    this.modal.close()
   }
 
 }
