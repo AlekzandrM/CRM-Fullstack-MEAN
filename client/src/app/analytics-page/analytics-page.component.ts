985ef70b782a4bfca25204fa7de1ff8ba91b55a1
AlekzandrM <mishenko.aleksandr@gmail.com>
2020-05-17 16:52:54 +0300

    Analytics-page. Installed chart.js. Revenue drawing.

--- client/src/app/analytics-page/analytics-page.component.ts
+++ client/src/app/analytics-page/analytics-page.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from
 import {AnalyticsService} from "../shared/services/analytics.service";
 import {AnalyticsPage} from "../shared/interfaces";
 import {Subscription} from "rxjs";
+import {Chart} from 'chart.js'
 
 @Component({
   selector: 'app-analytics-page',
@@ -21,8 +22,24 @@ export class AnalyticsPageComponent implements AfterViewInit, OnDestroy {
   }
 
   ngAfterViewInit(): void {
+    const gainConfig: any = {
+      label: 'Выручка',
+      color: 'rgb(255, 99, 132)'
+    }
+
     this.aSub = this.service.getAnalytics().subscribe((res: AnalyticsPage) => {
       this.average = res.average
+
+      gainConfig.labels = res.chart.map(item => item.date)
+      gainConfig.data = res.chart.map(item => item.gain)
+
+      // Получаем контекст канваса
+      const gainCtx = this.gainRef.nativeElement.getContext('2d')
+      gainCtx.canvas.height = '300px'
+
+      // Создаем
+      new Chart(gainCtx, createCharConfig(gainConfig))
+
       this.pending = false
     })
   }
@@ -33,4 +50,25 @@ export class AnalyticsPageComponent implements AfterViewInit, OnDestroy {
     }
   }
 
+}
+
+// Ф-я конфигурации всего графика
+function createCharConfig({labels, data, label, color}) {
+  return {
+    type: 'line',
+    options: {
+      responsive: true
+    },
+    data: {
+      labels,
+      datasets: [
+        {
+          label, data,
+          borderColor: color,
+          steppedLine: false,
+          fill: false
+        }
+      ]
+    }
+  }
 }
